b5e4f1f872374d497726bfd30f0655e6aec6f51a
Tarek Stelzle <tstelzle@laptop0733.client.jambit.com>
2021-12-13 14:55:54 +0100

    Changed Message

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+clonst express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hallo Welt!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
